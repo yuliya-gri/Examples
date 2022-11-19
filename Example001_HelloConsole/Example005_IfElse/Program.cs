55cfc765693da23fc0a53bdfac730555449c07b0
yuliya-gri <yuliya.grigoreva.1986@list.ru>
2022-11-19 22:25:11 +0400

    Example 005 IfElse

---
+++ Example001_HelloConsole/Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "юля")
+{
+    Console.WriteLine("Ура, это же ЮЛЯ!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
