426ad74bcb7334ce7649d5a5231a12c210bf5e18
yuliya-gri <yuliya.grigoreva.1986@list.ru>
2022-11-19 20:21:05 +0400

    Example 002 Hello User

---
+++ Example001_HelloConsole/Example002_HelloUser/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите ваше имя");
+string username = Console.ReadLine();
+Console.Write("Привет,");
+Console.Write(username);
